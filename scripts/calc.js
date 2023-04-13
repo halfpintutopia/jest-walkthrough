0e1c05c35abaae65da53d6c14db7e66945b92b38
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-13 12:54:12 +0200

    add boilerplate for walkthrough

--- scripts/calc.js
+++
@@ -1,8 +0,0 @@
-function addition(firstNum, secondNum) {
-  if (typeof firstNum == "number" && typeof secondNum == 'number') {
-    return firstNum + secondNum;
-  }
-  return "Please enter 2 numbers."
-}
-
-module.exports = addition;
